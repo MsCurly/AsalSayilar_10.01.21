d1fb181f359afedeaee14ae8c12494aa88726215
MsCurly <zuleyha_98@live.com>
2021-01-10 18:39:43 +0300

    ilk gonderme

---
+++ AsalSayilar_10.01.21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayilar_10._01._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 ile 10000 arasındaki asal sayılar ");
+            bool asalMi = true;
+            for (int sayi = 2; sayi < 10001; sayi++)
+            {
+                for (int bolen = 2; bolen < sayi; bolen++)
+                {
+                    if (sayi % bolen == 0 && sayi != bolen)
+                    {
+                        asalMi = false;
+                        break;
+                    }
+                }
+                if (asalMi)
+                {
+                    Console.Write("\t" + sayi);
+                }
+                asalMi = true;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
